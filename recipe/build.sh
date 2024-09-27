ae455dd27948d4694086054467179f44a02090a2
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-09-27 09:11:04 -0500

    more build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
+set -eux
 
 mkdir -p build/release
-cd build/release
-cmake
-    $CHECKSUM \
-    $CMAKE_ARGS \
-    -GNinja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_INSTALL_RPATH=${PREFIX}/lib \
-    -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
-    -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${CPPFLAGS}" \
-    -DTLSH_SHARED_LIBRARY=1 \
-    ../..
 
-cd ../..
+pushd build/release
+    cmake
+        $CMAKE_ARGS \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_INSTALL_RPATH=${PREFIX}/lib \
+        -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+        -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${CPPFLAGS}" \
+        -DTLSH_SHARED_LIBRARY=1 \
+        ../..
+popd
 
 make -C build DESTDIR="${PREFIX}" install
