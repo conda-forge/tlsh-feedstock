c8ca9d2c946147af6c610a0fc791cfb9919d83b0
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-09-27 09:23:25 -0500

    more build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -eux
 
-mkdir -p build/release
+mkdir -p build
 
-pushd build/release
+# adapted from https://gitlab.archlinux.org/archlinux/packaging/packages/tlsh/-/blob/main/PKGBUILD
+
+# remove garbage
+sed '/set(CMAKE_EXE_LINKER_FLAGS/d' -i CMakeLists.txt
+sed '/set(CMAKE_CXX_FLAGS/d' -i CMakeLists.txt
+sed 's|TLSH_SHARED_LIBRARY 0|TLSH_SHARED_LIBRARY 1|' -i src/CMakeLists.txt
+
+pushd build
     cmake \
         $CMAKE_ARGS \
         -GNinja \
@@ -12,9 +19,11 @@ pushd build/release
         -DCMAKE_INSTALL_RPATH=${PREFIX}/lib \
         -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
         -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${CPPFLAGS}" \
-        -DTLSH_SHARED_LIBRARY=1 \
         -DTLSH_CHECKSUM_1B=1 \
-        ../..
+        ..
 popd
 
-make -C build DESTDIR="${PREFIX}" install
+make \
+    -C build \
+    DESTDIR="${PREFIX}" \
+    install
