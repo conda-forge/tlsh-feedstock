41366c33b34ea17f2acc3b9c054e7d62fa7804b7
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-09-28 11:03:51 -0500

    clean up build, add dual license

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -eux
 
-# adapted from https://gitlab.archlinux.org/archlinux/packaging/packages/tlsh/-/blob/main/PKGBUILD
-
-# remove garbage
-sed '/set(CMAKE_EXE_LINKER_FLAGS/d' -i CMakeLists.txt
-sed '/set(CMAKE_CXX_FLAGS/d' -i CMakeLists.txt
-sed 's|TLSH_SHARED_LIBRARY 0|TLSH_SHARED_LIBRARY 1|' -i src/CMakeLists.txt
-
 mkdir -p build/release
 
 cd build/release
@@ -21,7 +14,6 @@ cmake ../.. \
     -DCMAKE_INSTALL_RPATH=${PREFIX}/lib \
     -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${CPPFLAGS}" \
-    -DTLSH_CHECKSUM_1B=1 \
     -DTLSH_SHARED_LIBRARY=1
 
 ninja
